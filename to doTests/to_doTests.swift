554195424675bf637a9c26024e1f096f5077b7a7
Ingrid Zippe <ingridzippe@gmail.com>
2026-06-24 19:49:05 -0700

    Initial Commit

---
+++ to doTests/to_doTests.swift
@@ -0,0 +1,19 @@
+//
+//  to_doTests.swift
+//  to doTests
+//
+//  Created by Ingrid Zippe on 6/24/26.
+//
+
+import Testing
+@testable import to_do
+
+struct to_doTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
